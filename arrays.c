ad7ce618bb58f087cae3effc6f18b80c570156f5
Liam Fisher <lfisher@redventures.com>
2019-12-21 16:18:17 -0500

    Added newline to printed text

--- arrays.c
+++ arrays.c
@@ -10,7 +10,7 @@ int main() {
 
   average = (grades[0] + grades[1] + grades[2]) / 3;
 
-  printf("The average of all three grades is: %d", average);
+  printf("The average of all three grades is: %d \n", average);
 
   return 0;
 }
