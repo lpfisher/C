e2960005706a11c4948fd8931e7276fa9de5ccb0
Liam Fisher <lfisher@redventures.com>
2019-12-21 15:59:25 -0500

    Added notes and variables.c

--- number.c
+++ number.c
@@ -3,5 +3,8 @@ main() {
   int count;
   puts("Please enter a number: ");
   scanf("%d", &count);
+  if (type) {
+    /* code */
+  }
   printf("The number is %d.\n", count);
 }
---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a = 3;
+  float b = 4.5;
+  double c = 5.25;
+  float sum;
+
+  sum = a + b + c
+
+printf("The sum of a, b, and c, if %f.", sum);
+return 0;
+
+}
