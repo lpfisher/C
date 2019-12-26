afe7e191a6445bf6bb0318689bec86645d6f3e13
Liam Fisher <lfisher@redventures.com>
2019-12-26 14:56:48 -0500

    corrected syntax errors in find-pi.c

--- source/find-pi.c
+++ source/find-pi.c
@@ -7,8 +7,9 @@ main () {
   if (num < 1) {
     printf("Please enter a number between 1 and 20.\n");
   } else if (num > 20) {
-    printf("Please enter a number between 1 and 20.\n")
+    printf("Please enter a number between 1 and 20.\n");
   } else {
     float pi = 3.14159265358979323846;
     printf("The value of pie to %d decimal places is %d.%df\n", num,pi,num);
   }
+}
