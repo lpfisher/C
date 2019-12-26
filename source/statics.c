3c4be9019299a0f808fc2d8028a28e9443f4ecf0
Liam Fisher <lfisher@redventures.com>
2019-12-26 13:23:25 -0500

    renamed notes files. Added statics files.

---
+++ source/statics.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum (int num) {
+  /*add code here*/
+  static int sum = 0;
+  sum += num;
+  return sum;
+}
+
+int main() {
+  printf("%d ",sum(55));
+  printf("%d ",sum(45));
+  printf("%d ",sum(50));
+  return 0;
+}
