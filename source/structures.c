4b00ce83142ea2f23626969e5d4dd14bfa24d8da
Liam Fisher <lfisher@redventures.com>
2019-12-26 14:39:37 -0500

    Added structures*

---
+++ source/structures.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*define the person structure with name and age parameters*/
+typedef struct {
+  char * name;
+  int age;
+} person;
+
+int main() {
+  person john;
+
+  /*test code to confirm appropriate parameters*/
+  john.name = "John";
+  john.age = 27;
+  printf("%s is %d years old. \n", john.name, john.age)
+}
