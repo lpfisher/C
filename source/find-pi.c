111eb7dd4dd153cb1ccaac29325fef97ec1937fb
Liam Fisher <lfisher@redventures.com>
2019-12-26 15:12:36 -0500

    adsf

--- source/find-pi.c
+++ source/find-pi.c
@@ -10,6 +10,8 @@ int main () {
     printf("Please enter a number between 1 and 20.\n");
   } else {
     float pi = 3.14159265358979323846f;
-        printf("The value of pie to %d decimal places is %.*f. \n", num,pi);
+    float pi_val = &pi.&num;
+        //printf("The value of pi to %d decimal places is %.*f. \n", num,pi);
+        printf("%d",pi-val);
   }
 }
